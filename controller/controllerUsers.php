c608f6b713936a82aab964bd76e82e7f91aac720
Coline-bd <coline.bertrand2@gmail.com>
2026-08-20 11:51:53 +0200

    feat: create class ControllerUsers

---
+++ controller/controllerUsers.php
@@ -0,0 +1,39 @@
+<?php
+//CONTROLLER
+
+class ControllerUsers
+{
+    private ModelUser $model;
+    // private ViewUser $view;
+
+    public function __construct(){
+        $this->model = new ModelUser(connect());
+    }
+    
+    public function render(){
+        //Appel du model pour récupération des données
+        $data = $this->model->findAll();
+
+        //Appel de la view pour effectuer l'affichage
+        $title = "Mes Utilisateurs";
+        include('./view/viewHeader.php');
+        include('./view/viewUser.php');
+        include('./view/viewFooter.php');
+    }
+
+}
+
+
+// function displayUsers(){
+//     //Creation d'un objet ModelUser
+//     $modelUser = new ModelUser(connect());
+
+//     //Appel du model pour récupération des données
+//     $data = $modelUser->findAll();
+
+//     //Appel de la view pour effectuer l'affichage
+//     $title = "Mes Utilisateurs";
+//     include('./view/viewHeader.php');
+//     include('./view/viewUser.php');
+//     include('./view/viewFooter.php');
+// }
