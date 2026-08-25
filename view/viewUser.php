b1e5b51a2fd2e6b0424e73e816eb1a51d74e75dd
Coline-bd <coline.bertrand2@gmail.com>
2026-08-25 09:23:43 +0200

    feat: modif viewUser avec ob_start

--- view/viewUser.php
+++ view/viewUser.php
@@ -6,6 +6,7 @@ class ViewUser{
     private array $dataUsers;
     private ViewHeader $header;
     private ViewFooter $footer;
+    private ?string $buffer;
 
     public function __construct(array $dataUsers,ViewHeader $header,ViewFooter $footer)
     {
@@ -15,21 +16,29 @@ class ViewUser{
         $this->footer=$footer;
 
     }
-
-    public function display():void{
+    public function launchBuffer():self{
+        ob_start();
         foreach($this->dataUsers as $row){
                     $this->listeUsers .="<li>Pseudo :".$row['pseudo']." - Email : ".$row['email']." - Role :".$row['role']."</li>";
-                };
-        echo '<main>
+                }; ?>
+         <main>
         <h1>Liste des utilisateurs</h1>
-        <ul>'. $this->listeUsers.'
+        <ul><?= $this->listeUsers ?> 
         </ul>
-    </main>';
+    </main>
+    <?php 
+    $this->buffer=ob_get_clean();
+    return $this;
+    }
+
+
+    public function display():void{
+        echo $this->buffer;
     }
     
     public function displayAll():void{
-        $this->header->display();
-        $this->display();
-        $this->footer->display();
+        $this->header->launchBuffer()->display();
+        $this->launchBuffer()->display();
+        $this->footer->launchBuffer()->display();
     }
 }
